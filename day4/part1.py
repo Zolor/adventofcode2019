514169b4ffdce204e91835583d23db6cdd6c3587
Erika Forslöf <erikaforslov@hotmail.com>
2019-12-04 21:22:53 +0100

    Part 1 success!

--- day4/part1.py
+++ day4/part1.py
@@ -2,25 +2,26 @@
 Two adjacent digits are the same (like 22 in 122345).
 Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
 '''
-
-input = str(range(109165, 576723))
-
-def code_cracker(input):
-	result = []
-	for i, j in enumerate(input[:-1]):
-	    if j  == input[i+1]: 
-       		if input[i] == input[i+1]:
-				result.append[input]
-	print(result)
-	
-#	for i in input:
-#		for letter in i:
-#			pos = int(i.index(letter))
-#			print(type(pos))
-#			p = str(i)
-#			if int(letter) == i[pos ++ 1]:
-#				result.append[i]
-#				break
-#	print(result)
-
-code_cracker(input)
+#lista = list(range(100, 200))
+lista = list(range(109165, 576723))
+def code_cracker(lista):
+    resultat = []
+    for i in lista:
+        n = str(i)
+        for k, j in enumerate(n):
+            if (k < len(n) - 1) and (j == n[k + 1]):
+                resultat.append(i)
+                break
+#    print(resultat)
+    remove = []
+    for i in resultat:
+        n = str(i)
+        for k, j in enumerate(n):
+            if (k < len(n) - 1) and (j > n[k + 1]):
+                remove.append(i)
+                break
+    for i in remove:
+        resultat.remove(i)
+    print(len(resultat))
+    
+code_cracker(lista)
