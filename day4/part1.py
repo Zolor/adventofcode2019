ff9bceea865024a278453ea40f8215147c179b7e
Erika Forslöf <erika.forslof@king.com>
2019-12-05 17:05:07 +0100

    Part 2 Day 4 still not working

--- day4/part1.py
+++ day4/part1.py
@@ -2,8 +2,8 @@
 Two adjacent digits are the same (like 22 in 122345).
 Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
 '''
-lista = list(range(100, 200))
-#lista = list(range(109165, 576723))
+#lista = list(range(100, 200))
+lista = list(range(109165, 576723))
 def code_cracker(lista):
     resultat = []
     for i in lista:
--- day4/part2.py
+++ day4/part2.py
@@ -2,27 +2,27 @@
 Two adjacent digits are the same (like 22 in 122345).
 Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
 '''
-#lista = list(range(1000, 1200))
-lista = list(range(109165, 576723))
-def code_cracker(lista):
-    resultat = []
-    for i in lista:
-        n = str(i)
-        for k, j in enumerate(n):
-            if ((k < len(n) - 1) and (j == n[k + 1])) and ((k < len(n) - 2) and (j != n[k + 2]) or (k > 0) and (j != n[k - 1])):
-#                print(i)
-                resultat.append(i)
-                break
-#    print(resultat)
-    remove = []
-    for i in resultat:
-        n = str(i)
-        for k, j in enumerate(n):
-            if (k < len(n) - 1) and (j > n[k + 1]):
-                remove.append(i)
-                break
-    for i in remove:
-        resultat.remove(i)
-    print(len(resultat))
-    
-code_cracker(lista)
+lista = list(range(150000, 159999))
+#lista = list(range(109165, 576723))
+resultat = []
+def code_cracker(number):
+	n = str(number)
+	for k, j in enumerate(n):
+		if (k == 5):
+			break
+		elif (k < len(n) - 1) and (j <= n[k + 1]):
+			print(n)
+			continue
+		else:
+			return(False)
+	for k, j in enumerate(n):
+		if (k < len(n) - 1) and (k < len(n) - 1) and (j == n[k + 1]) and (j != n[k + 2]):
+			return(True)
+		else:
+			break
+
+for i in lista:
+	if code_cracker(i):
+		resultat.append(i)
+print(resultat)
+print(len(resultat))
