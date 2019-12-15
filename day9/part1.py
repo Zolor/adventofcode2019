a155ad1fb544d30625099fe7690e4c711df1963f
Erika Forslöf <erikaforslov@hotmail.com>
2019-12-15 11:46:48 +0100

    Day9 part 1 and 2 done!

---
+++ day9/part1.py
@@ -0,0 +1,9 @@
+from intcode1 import intcode_program
+import collections
+
+with open('input.txt') as input:
+	for i in input.readlines():
+		code = [int(part) for part in i.strip().split(',')]
+		intcode_dict = collections.defaultdict(int, enumerate(code))
+
+print(intcode_program(intcode_dict))
