8cca8342f821f0663d3f861ba358ca86c73a7f3f
Erika Forslöf <erikaforslov@hotmail.com>
2019-12-18 23:03:38 +0100

    day 16 part2 working!

--- day16/part2.py
+++ day16/part2.py
@@ -1,40 +1,23 @@
 import time
 start_time = time.time()
 
-fet_pattern = []
-fet_pattern_finns = False
 fet_lista = []
 tmp_lista = []
 with open('input.txt') as input:
 	for i in input.read():
 		i = int(i)
 		tmp_lista.append(i)
-	fet_lista = [val for val in tmp_lista for _ in range(10000)]
+	fet_lista = tmp_lista * 10000
 
-base_pattern = [0, 1, 0, -1]
-def pattern_maker(input, iter):
-	if iter == 0:
-		end_pattern = base_pattern
-	else:
-		end_pattern = [val for val in base_pattern for _ in range(0, (iter + 1))]
-	end_pattern = end_pattern * (len(input) // len(end_pattern) + 1)
-	end_pattern.remove(end_pattern[0])
-	return(end_pattern)
-
-def runner(input, fet_pattern_finns):
-	output = []
-	for i in range(len(input)):
-		if fet_pattern_finns == False:
-			pattern = pattern_maker(input, i)
-			fet_pattern.append(pattern)
-		zip_list = [a*b for a,b in zip(input,fet_pattern[i])]
-		output.append(abs(sum(zip_list)) % 10)
-	fet_pattern_finns = True
-	return output
-
+offset = fet_lista[0:7]
+offset = int(str(offset[0]) + str(offset[1]) + str(offset[2]) + str(offset[3]) + str(offset[4]) + str(offset[5]) + str(offset[6]))
+fet_lista = fet_lista[offset:len(fet_lista)]
 for x in range(100):
-	tmp_list = runner(fet_lista, fet_pattern_finns)
-	fet_lista = tmp_list
-output = ''.join([str(fet_lista[i]) for i in range(len(fet_lista)) if i < 8])
-print(output)
+	tmp = 0
+	for i in reversed(range(0, len(fet_lista))):
+		tmp += fet_lista[i]
+		fet_lista[i] = tmp % 10
+
+print(''.join([str(c) for c in fet_lista[:8]]))
+
 print("--- %s seconds ---" % (time.time() - start_time))
