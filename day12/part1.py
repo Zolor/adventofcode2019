ba426cbea7e2ae88e239bb704df4558b1ea3dc6c
Erika Forslöf <erikaforslov@hotmail.com>
2020-01-04 13:39:32 +0100

    day12 part1 done!

--- day12/part1.py
+++ day12/part1.py
@@ -1,13 +1,43 @@
-moons = {
-"moon1" : {"x" : 5, "y" : 4, "z" : 4, "vel_x" : 0, "vel_y" : 0, "vel_z" : 0},
-"moon2" : {"x" : -11, "y" : -11, "z" : -3, "vel_x" : 0, "vel_y" : 0, "vel_z" : 0},
-"moon3" : {"x" : 0, "y" : 7, "z" : 0, "vel_x" : 0, "vel_y" : 0, "vel_z" : 0},
-"moon4" : {"x" : -13, "y" : 2, "z" : 10, "vel_x" : 0, "vel_y" : 0, "vel_z" : 0}
-}
-def motion(moons):
-	for _ in range(1000):
-		for k, v in moons.items():
-			for x, y in v.items():
-				
+from typing import NamedTuple
 
-motion(moons)
+class Moon(NamedTuple):
+	x: int
+	y: int
+	z: int
+class Vel(NamedTuple):
+	x: int
+	y: int
+	z: int
+moons = {"Io":Moon(5, 4, 4),"Europa":Moon(-11, -11, -3),"Ganymede":Moon(0, 7, 0),"Callisto":Moon(-13, 2, 10)}
+#Test
+#moons = {"Io":Moon(-8, -10, 0),"Europa":Moon(5, 5, 10),"Ganymede":Moon(2, -7, 3),"Callisto":Moon(9, -8, -3)}
+vel = {"Io":Vel(0,0,0), "Europa":Vel(0,0,0), "Ganymede":Vel(0,0,0), "Callisto":Vel(0,0,0)}
+
+def moon_velocity(moons):
+	total_energy = 0
+	def velocity(moon, compare):
+		if moon > compare:
+			return -1
+		elif moon < compare:
+			return +1
+		elif moon == compare:
+			return 0
+	for _ in range(1000):
+		tmp_moons = {**moons}
+		for moon_name, moon_value in tmp_moons.items():
+			for compare_name, compare_value in tmp_moons.items():
+				if moon_name == compare_name:
+					continue
+				else:
+					tmp_vel_x = vel.get(moon_name).x + velocity(moon_value.x, compare_value.x)
+					tmp_vel_y = vel.get(moon_name).y + velocity(moon_value.y, compare_value.y)
+					tmp_vel_z = vel.get(moon_name).z + velocity(moon_value.z, compare_value.z)
+					vel[moon_name] = Vel(tmp_vel_x,tmp_vel_y,tmp_vel_z)
+			moons[moon_name] = Moon((moon_value.x + vel.get(moon_name).x),(moon_value.y + vel.get(moon_name).y),(moon_value.z + vel.get(moon_name).z))
+	for moon_name in moons:
+		pot = abs(moons.get(moon_name).x) + abs(moons.get(moon_name).y) + abs(moons.get(moon_name).z)
+		kin = abs(vel.get(moon_name).x) + abs(vel.get(moon_name).y) + abs(vel.get(moon_name).z)
+		total_energy += pot * kin
+	return total_energy
+
+print(moon_velocity(moons))
